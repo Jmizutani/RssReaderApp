56a5ca77ffa9907633d5c805dd8fb0b80102b444
Junya Mizutani <mizutani@cyber.t.u-tokyo.ac.jp>
2019-01-04 23:40:42 +0900

    Initial Commit

---
+++ RssReaderApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RssReaderApp
+//
+//  Created by 水谷純也 on 2019/01/04.
+//  Copyright © 2019 水谷純也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
